6f472be4b7fbbca0f189e2774d4abdb3ef08caf3
anuragsingh2207 <anuragsingh2207@gmail.com>
2022-10-10 23:14:41 +0530

    Added Mathemamtical operators

---
+++ python101.py
@@ -0,0 +1,70 @@
+print("hello world")
+
+# Variable Assignments
+var1 = 5
+
+# Variable Re-assignments (Updating the value of the variable)
+var1 = 7
+
+'''
+++Variable Naming Rules++
+
+++Cannot++
+4v1 => Variable name cannot start with number
+var 1 => Variable name cannot have a space
+$var1 => Variable name cannot start with special characters
+
+
+++Can++
+myName => Variable name can have camel case characters
+my_name => Variable name can have snake case characters (Recommended)
+var12 => Variable name can have the numbers but not begining
+
+'''
+
+# Variable Data Types
+
+float_1 = 1.2345
+
+int_1 = 25
+
+boolean_1 = True
+
+float_1 = 29
+
+# Mathematical Operators
+
+add = 4 + 5  # 9
+subtract = 5 - 3  # 2
+multiply = 5 * 3  # 15
+divide = 5 / 2  # 2.5
+
+exponentiation = 4 ** 4  # 256
+floor_division = 16 // 5  # 3
+modulo = 7 % 3  # 1
+
+# Assignment Operators
+
+add_assign = 7
+add_assign += 5  # 12
+add_assign = add_assign + 5  # 12
+
+# Assignment Operators are available for all the above-mentioned Mathematical Operators
+
+'''
+Operator Precedence
+
+()
+**
+%, /, // and *
++ and -
+
+For same precedence of operators opertaions happen from left to right 
+
+(9-7) * 2 ** 3 + 10 % 6 // -1 * 2 -1
+2 * 2 ** 3 + 10 % 6 // -1 * 2  -1
+2 * 8 + 10 % 6 // -1 * 2  -1
+16 + -8 -1
+7
+
+'''
