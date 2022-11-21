bb4a6a6816d5fb771db0292b3696c9d2b6bd5e1d
anuragsingh2207 <anuragsingh2207@gmail.com>
2022-11-21 08:09:50 +0530

    Added program for conditional operators

---
+++ python_for_absolute_beginners/flow_control/conditional_operators.py
@@ -0,0 +1,14 @@
+print("1 > 2 : "+str(1 > 2))
+print("5.70 < 6 : "+str(5.70 < 6))
+print("1 == 2 : "+str(1 == 2))
+print("2 != 3 : "+str(2 != 3))
+print("6 <= 7 : "+str(6 <= 7))
+print("2.87 >= 4.87 : "+str(2.87 >= 4.87))
+
+fruit1 = "apple"
+fruit2 = "mango"
+
+print("apple == mango : "+str(fruit1 == fruit2))
+print("hello == world : "+ str("hello" == "world"))
+
+print("hello == Hello : "+ str("hello" == "Hello"))
