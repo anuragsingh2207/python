49892d620731d8abf1193f3417bf7a560b39ba00
anuragsingh2207 <anuragsingh2207@gmail.com>
2022-11-21 07:36:59 +0530

    Updated the formatting of the code

--- python_for_absolute_beginners/maths.py
+++ python_for_absolute_beginners/maths.py
@@ -5,14 +5,14 @@ subtract = 5 - 3  # 2
 multiply = 5 * 3  # 15
 divide = 5 / 2  # 2.5
 
-exponentiation= 4 * * 4  # 256
+exponentiation= 4 ** 4  # 256
 floor_division= 16 // 5  # 3
 modulo= 7 % 3  # 1
 
 # Assignment Operators
 
 add_assign = 7
-add_assign+ = 5  # 12
+add_assign+= 5  # 12
 add_assign = add_assign + 5  # 12
 
 # Assignment Operators are available for all the above-mentioned Mathematical Operators
--- python_for_absolute_beginners/print.py
+++ python_for_absolute_beginners/print.py
@@ -5,5 +5,5 @@ floor_division = 16 // 5  # 3
 
 print(floor_division)
 print(20189)
-print(4 * * 4 )
+print(4 ** 4 )
 print(True)
