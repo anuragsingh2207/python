b02367c9dfca77016885913359a02d79cc584492
anuragsingh2207 <anuragsingh2207@gmail.com>
2022-11-21 07:44:47 +0530

    Added program for variables local & global scope

--- python_for_absolute_beginners/function/function_var_scope.py
+++ python_for_absolute_beginners/function/function_var_scope.py
@@ -7,14 +7,33 @@ def retrun_string():
     
 string = "Outside function Global Variable"
 
+print("\n+++First Program+++")
+
 print(string)
 
 print(retrun_string())
+print("\n\n")
 
 
 #Using Global variable within the function scope
 
-def 
+def print_var():
+    print(ex1)
 
 
-ex1= 
+ex1 = "Global Variable"
+print("+++Second Program+++")
+print_var() 
+
+
+#Same name of varaibles but in different scope
+
+def sum(a, b):
+    sum = a + b
+    return sum
+
+a = 2 
+b = 3 
+
+print("\n+++Third Program+++")
+print("Sum of a & b is: "+str(sum(a,b)))
