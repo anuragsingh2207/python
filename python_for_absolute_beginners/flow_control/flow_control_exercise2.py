0f6a307054dcb515841c23beca910987a13bc8ef
anuragsingh2207 <anuragsingh2207@gmail.com>
2022-12-19 10:31:42 +0530

    Added flow_control exercise

---
+++ python_for_absolute_beginners/flow_control/flow_control_exercise2.py
@@ -0,0 +1,27 @@
+num = int(input("Enter a number between 1 to 10:"))
+
+
+if num == 1:
+    print("You entered I")
+elif num == 2:
+    print("You entered I")
+elif num == 3:
+    print("You entered III")
+elif num == 4:
+    print("You entered IV")
+elif num == 5:
+    print("You entered V")
+elif num == 6:
+    print("You entered VI")
+elif num == 7:
+    print("You entered VII")
+elif num == 8:
+    print("You entered VIII")
+elif num == 9:
+    print("You entered IX")          
+elif num == 10:
+    print("You entered X")   
+elif num  < 1:
+    print("You entered number less than 1")   
+else:
+    print("You entered number greater than 10")   
