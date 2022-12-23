85da5b3d65e8cad222b817ba5467d213be8d6940
anuragsingh2207 <anuragsingh2207@gmail.com>
2022-12-23 20:39:21 +0530

    Added Python factorial exercise program

--- python_for_absolute_beginners/loops/for_loop_range_exercise3.py
+++ python_for_absolute_beginners/loops/for_loop_range_exercise3.py
@@ -1,7 +1,10 @@
-num = int(input("Enter any positive integer:"))
-factorial =1 
 
-for i in range(1, num+1):
-    factorial = factorial * i
+def factorial_calculate(fac_num):
+    factorial =1 
+    for i in range(1, fac_num+1):
+        factorial = factorial * i
+    return factorial    
 
-print("Factorial of " + str(num) + " is " + str(factorial))
+print("Factorial of 3 is " + str(factorial_calculate(3)))
+print("Factorial of 3 is " + str(factorial_calculate(4)))
+print("Factorial of 3 is " + str(factorial_calculate(7)))
